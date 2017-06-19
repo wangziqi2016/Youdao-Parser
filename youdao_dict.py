980b65f4a90f9aaf5db04ffceb9247b32cdb800a
Ziqi Wang <675973885@qq.com>
2017-06-19 16:46:19 -0400

    Update usage string

--- youdao_dict.py
+++ youdao_dict.py
@@ -1765,15 +1765,15 @@ The following is used without specifying the [word]
 -h/--help         Display this message
 --install [dir]   Install this as an utility, "define". 
                   Optional argument specifies the location. 
---uninstall       Uninstall the "define" utility. This removes the first "define"
-                  utility that appears under PATH
+--uninstall       Uninstall the "define" utility. This removes the file returned
+                  by executing "--ls-define", which is the installation path
 
 --trim-cache [#]  Remove cache contents until there are [#] of entry/-ies left
                   The number must be an integer greater than or equal to 0
                   Default value is 0, which means deleting all contents from 
                   the cache
 --ls-cache        List words in the cache. One word each line
---ls-define       Print the absolute file name of the utility
+--ls-define       Print the absolute file name of the utility (i.e. the installation path)
 --ls-dir          Print the directory of this file
 
 -i/--interactive  Start in interactive mode; Press ESC to exit
